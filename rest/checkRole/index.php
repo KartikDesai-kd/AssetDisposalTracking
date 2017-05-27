59e6f62f5645c30ff7781d2860cbf15bb63d3dfb
KartikDesai-kd <kartikdesai3@gmail.com>
2017-05-27 18:15:35 +0530

    Add files via upload

--- rest/checkRole/index.php
+++ rest/checkRole/index.php
@@ -1,1 +1,11 @@
-
+<?php
+  $user = $_SERVER['REMOTE_USER'];
+  $user = explode("\\",$user);
+  $user = $user[1];
+  // $user = 'kdesai';
+  if ($user == 'bdusome' || $user == 'jcarlin' ||  $user == 'kadesai') {
+    echo json_encode(array("Role"=>"Admin","User"=>$user));
+  } else {
+    echo json_encode(array("Role"=>"User","User"=>$user));
+  }
+ ?>
