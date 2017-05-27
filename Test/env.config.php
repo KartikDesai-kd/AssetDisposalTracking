8b26e13dbb6dc3d75696b857c89c9be958bf2c04
KartikDesai-kd <kartikdesai3@gmail.com>
2017-05-27 18:11:56 +0530

    Add files via upload

---
+++ Test/env.config.php
@@ -0,0 +1,39 @@
+<?php
+
+  //##############################################################################
+  //##############################################################################
+  //##############################################################################
+  //##############################################################################
+  //class envConfig is the default environment configuration variables
+  class envConfig {
+    public $URL = "https://jiraqa.rim.net";
+    public $UID = "svcAcctBAT";
+    public $PWD = "j_WlGN]fKjde%?J:";
+    public $iTopURL = "http://qa.cmdb.rim.net/webservices/rest.php?version=1.0&json_data=";
+    public $LoggingDB = '[BAT_Logging_DEV]';
+    public $metaDB = '[BAT_DEV]';
+    public $CarrierDB = '[CarrierData_Dev]';
+    public $ewasteDB = '[ewaste]';
+    public $sqlServer = 'tlsql002ykf';
+    public $sqlConnection = array(
+      "UID"=>'BatWebUser',
+      "PWD"=>'f>m^x4P3KC5h#)UD.4Y~Bn+F',
+      "Database"=>"master",
+      "ReturnDatesAsStrings"=>true
+    );
+    public $wikiPages = 'https://wikis.rim.net/plugins/viewsource/viewpagesrc.action?pageId=';
+    public $wikiUID = "batman";
+    public $wikiPWD = "@pn$:@.clVmigKWF";
+    public $TLSQLServer = 'tlsql002ykf';
+    public $TLSQLconnectionInfo = array(
+      "UID"=>"BatWebUser",
+      "PWD"=>'f>m^x4P3KC5h#)UD.4Y~Bn+F',
+      "Database"=>"master",
+      "ReturnDatesAsStrings"=>true
+    );
+    public $loggingDB = '[BAT_Logging_DEV]';
+    public $iTopUID = "svc_asset_tracker@blackberry.com";
+    public $iTopPWD = "AssetTrack3r";
+
+  }//end of class ticketing
+?>
